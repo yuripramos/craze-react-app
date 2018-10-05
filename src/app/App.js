7a5ece35a89c18d365c9e11ed524c79fe20e97d1
Yuri Ramos <yuri.ramos@accenture.com>
2018-10-05 20:34:08 -0300

    adding prettier ignore, styled components and a few updates - bootstraping part 2/5

--- src/app/App.js
+++ src/app/App.js
@@ -1,22 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
 import "./App.css";
 import Footer from "../components/footer/Footer.js";
 import ComponentA from "../components/component_a/componentA.js";
 import ComponentB from "../components/component_b/componentB.js";
 import Header from "../components/header/Header.js";
 
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'State 1',
-      lastname: 'State 2',
+      name: "State 1",
+      lastname: "State 2"
     };
   }
   render() {
     return (
       <div>
+        <Wrapper>
+          <Title>Hello World, this is my first styled component!</Title>
+        </Wrapper>
         <div className={`container`}>
           <div className="row">
             <Header />
@@ -35,6 +50,5 @@ class App extends Component {
     );
   }
 }
-
 
 ReactDOM.render(<App />, document.getElementById("app"));
